522c6de4cc912d3b873263751b1547fd285542ce
Marcia Torrico <marciatorrico@gmail.com>
2019-03-30 20:33:08 -0700

    clean data update for word encoding

--- src/clean_data.py
+++ src/clean_data.py
@@ -76,3 +76,4 @@ with open(output_text_file[:-4] + "json", "w") as out:
 ### Create lable for classification
 """Set 0 for the reviews having an overall of 1.0 (neg class),
 Set 1 for the reviews having an overall of 5.0 (pos class)."""
+   
